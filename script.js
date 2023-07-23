3fa2809793c75267f15129cc10d1645bdd5f3a6d
Ben <62263898+BenBissTech@users.noreply.github.com>
2023-07-23 20:26:23 +0100

    Button and basic computation functionality
    Added initial calculator functionality within JS.
    
    Will need to test use cases and in order to clarify rules for the correct display depending on operation and operands.

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
+
 class Calculator {
     constructor(prevOpTextElement, currOpTextElement) {
         this.prevOpTextElement = prevOpTextElement;
         this.currOpTextElement = currOpTextElement;
+        this.clear();
     }
 
+    // method used to clear calculator inputs and display
     clear() {
-
+        this.currOp = "";
+        this.prevOp = "";
+        this.op = undefined;
     }
 
+    // method used to delete last input on calcultor
     delete() {
-
+        this.currOp = this.currOp.toString().slice(0,-1);
     }
 
+    // method used to add pressed number to current current operand
     appendNumber(number) {
-
+        if (number === '.' && this.currOp.includes('.')) return;
+        this.currOp = this.currOp.toString() + number.toString();
     }
 
+    // method used to handle current operation (last selected operation)
     chooseOperation(operation) {
+        if (this.currOp === '') return;
+        if (this.prevOp !== '') this.compute(); 
 
+        this.op = operation;
+        this.prevOp = this.currOp;
+        this.currOp = '';
     }
 
+    // method used to perform calculation
     compute() {
+        let computation;
+        const prev = parseFloat(this.prevOp);
+        const curr = parseFloat(this.currOp);
 
+        if (isNaN(prev) || isNaN(curr)) return
+        
+        // switch case statement for handling 4 different operations
+        switch (this.op) {
+            case '+':
+                computation = prev + curr;
+                break;
+            case '-':
+                computation = prev - curr;
+                break;
+            case '*':
+                computation = prev * curr;
+                break;
+            case '÷':
+                computation = prev / curr;
+                break;
+            default:
+                return;
+        }
+        this.currOp = computation;
+        this.op = undefined;
+        this.prevOp = '';
     }
 
-    updateChange() {
-        
+    // method used to update the display on the calculator
+    handleChange() {
+        this.currOpTextElement.innerText = this.currOp;
+        this.prevOpTextElement.innerText = this.prevOp;
     }
 }
 
@@ -38,6 +80,42 @@ const opButtons = document.querySelectorAll('[data-op]')
 const equalsButton = document.querySelector('[data-equals]')
 const delButton = document.querySelector('[data-del]')
 const allClearButton = document.querySelector('[data-all-clear]')
-const prevOpTextElement = document.querySelector('[prev-op]')
-const currOpTextElement = document.querySelector('[curr-op]')
+const prevOpTextElement = document.querySelector('[data-prev-op]')
+const currOpTextElement = document.querySelector('[data-curr-op]')
 
+const calculator = new Calculator(prevOpTextElement,currOpTextElement);
+
+// handles number button presses
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.handleChange();
+        console.log("num pressed: " +button.innerText);
+    })
+})
+
+// handles operation button presses
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.handleChange();
+    })
+})
+
+// handles equal button presses
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.handleChange();
+})
+
+// handles AC button presses
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.handleChange();
+})
+
+// handles DEL button presses
+delButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.handleChange();
+})
